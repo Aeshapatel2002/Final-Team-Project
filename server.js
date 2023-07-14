d1974b5c59651d0338f7e9a241c464e9512b6a0e
deepPatel3007 <deep307patel@gmail.com>
2023-07-14 17:30:50 -0400

    Home Page Updated

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express');
-const app = express;
+const app = express();
 const path = require('path');
+
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
+app.set('view engine', 'ejs');
 
 //middleware and static files 
 app.use(express.static('public'));
-app.use(express.static('node_modules'));
-app.use(express.static(path.join(__dirname,'../node_modules')));
-
-//View Engine Set-Up
-app.set('views',path.join(__dirname,'./views'));
-app.set('vieew engine','ejs');
 
 
-app.get('/',(req,res)=>{
-    res.render('home.ejs', {title: 'Home'});
+app.get('/', (req, res) => {
+  res.render('home.ejs',{ title: 'Home' });
 });
 
-app.get('/about',(req,res)=>{
-    res.render('about.ejs', {title: 'About Us'});
+app.get('/about', (req, res) => {
+  res.render('aboutme', { title: 'About Us' });
 });
 
-app.get('/services',(req,res)=>{
-    res.render('services.ejs', {title: 'Services'});
+app.get('/project', (req, res) => {
+  res.render('project', { title: 'Project' });
 });
 
-app.get('/contact',(req,res)=>{
-    res.render('contact.ejs', {title: 'Contact'});
+app.get('/services', (req, res) => {
+  res.render('services', { title: 'Services' });
 });
 
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact' });
+});
 
-//listening to port  
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
-})  
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
